0d34187679216d3cb96eed9c862958c7f6eeb14f
meepingsnesroms <guicrith@gmail.com>
2016-10-19 16:43:09 -0700

    First commit

---
+++ src/herdfx.h
@@ -0,0 +1,10 @@
+#include <retro_inline.h>
+#include <retro_miscellaneous.h>
+#include <rthreads/rthreads.h>
+#include <rthreads/rsemaphore.h>
+
+int startSFXcuttlefish();
+void stopSFXcuttlefish();
+
+void sfxrun_cuttle();
+void sfxwait_cuttle();
